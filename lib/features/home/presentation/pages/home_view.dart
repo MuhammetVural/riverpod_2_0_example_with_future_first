943bcb19425906dfa607479049f7fc058045dd0a
Muhammet Vural <muhammetvural94@gmail.com>
2023-04-11 18:00:18 +0300

    provider kısmı

--- lib/features/home/presentation/pages/home_view.dart
+++ lib/features/home/presentation/pages/home_view.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpod_2_example/features/home/presentation/riverpod/home_provider.dart';
+import 'package:riverpod_2_example/features/home/presentation/riverpod/home_state.dart';
 
-class HomeView extends StatelessWidget {
+  final homeProvider = NotifierProvider<HomeProvider, HomeState>(
+    () => HomeProvider(),
+  );
+
+class HomeView extends ConsumerWidget {
   const HomeView({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final provider = ref.watch(homeProvider);
     return Scaffold(
-      body: Center(
-        child: Text('Hello World!'),
-      ),
+      body: ListView.builder(
+        itemCount: provider.categories,
+        itemBuilder: (BuildContext context, int index) {
+          return ;
+        },
+      ),,
     );
   }
 }
