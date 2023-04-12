3264cda88bc1ad9ccca443027597c4d1a97596ca
Eda <eroglueda20@gmail.com>
2023-04-12 13:58:26 +0300

    f

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MainApp extends StatelessWidget {
     return const MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text(''),
+          child: Text(' bbhj'),
         ),
       ),
     );
